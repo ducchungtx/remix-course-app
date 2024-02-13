f1a7be874f28b18cc268143c61a9a58e9948e7f0
Chung Nguyen <zrmedia9@gmail.com>
2024-02-13 18:23:49 +0700

    Add Prisma dependency and remove unused route

--- app/routes/$.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "@remix-run/node";
-
-export const loader = ({ params }: { params: any }) => {
-  if (params['*'] === 'exp') {
-    return redirect('/expenses');
-  }
-
-  throw new Response('Not found', { status: 404 });
-}
