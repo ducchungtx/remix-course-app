9c92a2be927accb0afbf869e65769b2a1bb394df
Chung Nguyen <zrmedia9@gmail.com>
2024-02-14 09:26:17 +0700

    Refactor ExpenseForm component

--- app/components/expenses/ExpenseForm.jsx
+++ app/components/expenses/ExpenseForm.jsx
@@ -1,11 +1,29 @@
-import { Link, useActionData } from '@remix-run/react';
+import { Form, Link, useActionData, useNavigation } from '@remix-run/react';
 
 function ExpenseForm() {
   const today = new Date().toISOString().slice(0, 10); // yields something like 2023-09-10
   const validationErrors = useActionData();
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state !== 'idle';
+
+  // const submit = useSubmit();
+
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   // perform form validation
+  //   submit(event.target, {
+  //     // action: '/expenses/add',
+  //     method: 'post',
+  //   });
+  // };
 
   return (
-    <form method="post" className="form" id="expense-form">
+    <Form
+      method="post"
+      className="form"
+      id="expense-form"
+      // onSubmit={handleSubmit}
+    >
       <p>
         <label htmlFor="title">Expense Title</label>
         <input type="text" id="title" name="title" required maxLength={30} />
@@ -36,10 +54,12 @@ function ExpenseForm() {
         </ul>
       )}
       <div className="form-actions">
-        <button>Save Expense</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? 'Saving...' : 'Save Expense'}
+        </button>
         <Link to="..">Cancel</Link>
       </div>
-    </form>
+    </Form>
   );
 }
 
