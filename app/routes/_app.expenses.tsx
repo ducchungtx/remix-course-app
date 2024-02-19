9623d63e566aa533aa0e6cc4ee8920527480a250
Chung Nguyen <zrmedia9@gmail.com>
2024-02-19 20:23:22 +0700

    Add conditional rendering for expenses list

--- app/routes/_app.expenses.tsx
+++ app/routes/_app.expenses.tsx
@@ -9,6 +9,8 @@ import { getExpenses } from '~/data/expenses.server';
 export default function ExpensesLayout() {
   const { expenses } = useLoaderData<{ expenses: [] }>();
 
+  const hasExpenses = expenses && expenses.length > 0;
+
   return (
     <>
       <Outlet />
@@ -23,7 +25,13 @@ export default function ExpensesLayout() {
             <span>Load Raw Data</span>
           </a>
         </section>
-        <ExpensesList expenses={expenses} />
+        {hasExpenses && <ExpensesList expenses={expenses} />}
+        {!hasExpenses && (
+          <section>
+            <h1>No expenses found</h1>
+            <p>Start <Link to="add">adding some</Link> today.</p>
+          </section>
+        )}
       </main>
     </>
   );
@@ -31,11 +39,11 @@ export default function ExpensesLayout() {
 
 export const loader = async () => {
   const expenses = await getExpenses();
-  if (!expenses || expenses.length === 0) {
-    throw json(
-      { message: 'No expenses found' },
-      { status: 404, statusText: 'No expenses found' }
-    );
-  }
+  // if (!expenses || expenses.length === 0) {
+  //   throw json(
+  //     { message: 'No expenses found' },
+  //     { status: 404, statusText: 'No expenses found' }
+  //   );
+  // }
   return json({ expenses });
 };
